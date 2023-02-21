4f9f3eb630f8831bc1919d562bbf9d720f214503
Shreya Verma <shreya_verma@mckinsey.com>
2023-02-21 10:07:27 +0530

    feat: Migration, model and seed for blog posts

---
+++ models/blogPost.js
@@ -0,0 +1,37 @@
+const {
+  Model,
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class BlogPost extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate() {
+      // define association here
+    }
+  }
+  BlogPost.init({
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.STRING,
+    },
+    date: DataTypes.DATE,
+    readingTime: {
+      field: 'reading_time',
+      type: DataTypes.STRING,
+    },
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    claps: DataTypes.INTEGER,
+    liked: DataTypes.BOOLEAN,
+    image: DataTypes.STRING,
+  }, {
+    sequelize,
+    modelName: 'blogPosts',
+  });
+  return BlogPost;
+};
